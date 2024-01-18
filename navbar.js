34f1d52658e4d4eff5af219426d16a20759cd96f
LokiNasty <150024507+LokiNasty@users.noreply.github.com>
2024-01-18 13:28:42 +0200

    landing and nav

--- navbar.js
+++ navbar.js
@@ -1,19 +1,8 @@
-function darkMode() {
-    let bdVal = document.getElementById("bg").classList.value;
-    console.log(bdVal);
-    
-    if (bdVal === "light") {
-        document.getElementById("bg").setAttribute("class", "dark");
-        document.getElementById("bg").setAttribute("value", "1");
-        document.getElementById("darkLbl").innerHTML= "Light mode";
-    } else if (bdVal === "dark") {
-        document.getElementById("bg").setAttribute("class", "light");
-        document.getElementById("bg").setAttribute("value", "0");
-        document.getElementById("darkLbl").innerHTML= "Dark mode";
+function myFunction() {
+    var x = document.getElementByClass("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
     } else {
-        document.getElementById("bg").setAttribute("class", "dark");
-        document.getElementById("bg").setAttribute("value", "1");
-        
+      x.className = "topnav";
     }
-}
-
+  }
